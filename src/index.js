b285ba671277f0b0b6598b0c7081fff2daf8a5ed
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-01-22 15:52:40 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    #secure = url.startsWith("https") ? true : false;
+  }
+
+  isSecure() {
+    return #secure;
+  }
+
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    #secure = newUrl.startsWith("https") ? true : false;
+  }
+}
 
 module.exports = { API }
