46bc590c5af6123c94d382330cc226393225dd2b
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-01-24 15:15:50 +0530

    new

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ class API {
     constructor(url, method = 'GET') {
         this.url = url;
         this.method = method;
-        this.#secure = this.url.startsWith('https') ? true : false;
-    }
 
-    isSecure() {
-        return this.#secure;
-    }
+        let secure;
+        if (this.url.startsWith('https')) {
+            secure = true;
+        } else {
+            secure = false;
+        }
 
-    updateURL(url) {
-        this.url = url;
-        this.#secure = this.url.startsWith('https') ? true : false;
+        this.isSecure = function() {
+            return secure;
+        }
+
+        this.updateURL = function(newUrl) {
+            this.url = newUrl;
+            if (this.url.startsWith('https')) {
+                secure = true;
+            } else {
+                secure = false;
+            }
+        }
     }
 }
+
 
 
 module.exports = { API }
