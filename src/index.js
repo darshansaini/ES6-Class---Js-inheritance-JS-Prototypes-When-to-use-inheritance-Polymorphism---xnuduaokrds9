d1ac759fee4d2ad95fd77ff87c4fbf2c5de36fc0
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-01-24 15:09:50 +0530

    try

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ class API {
   constructor(url, method = 'GET') {
     this.url = url;
     this.method = method;
-    var obj.#secure = true;
-    obj.#secure = url.startsWith("https") ? true : false;
+    
+    #secure = url.startsWith("https") ? true : false;
   }
 
   isSecure() {
-    return obj.#secure;
+    return #secure;
   }
 
   updateUrl(newUrl) {
     this.url = newUrl;
-    obj.#secure = newUrl.startsWith("https") ? true : false;
+    #secure = newUrl.startsWith("https") ? true : false;
   }
 }
 
